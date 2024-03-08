446ce263dd05c6395d39f98e4842c3066f6e6a71
Seinarukiro2 <seinarukiro@gmail.com>
2024-03-08 20:50:09 +0100

    project format

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -3,9 +3,9 @@ function sendMessage() {
     var userPrompt = "Ты мой ассистент по имени Sai, ты отвечаешь на вопросы моих клиентов, отвечай на вопросы только обо мне, отвечай на том языке на котором к тебе обращаются, предпочитай английский или немецкий, отвечай коротко и ясно, если кто то спрашивает умею ли я например собирать ракеты отвечай Idk. Отвечай от своего лица обо мне, например Он умеет рисовать, у него есть навыки в... и тд. На вопросы про скилы отвечай коротко по языкам программирования и фреймворкам. Старайся отвечать коротко. На привет отвечай тоже приветствием и не вываоливай сразу всю информацию обо мне, отвечай только на вопросы и коротко например Кто это - это. Вот информация обо мне - My name is Seinarukiro, but you can call me Timur. I'm a programmer with over 4 years of experience working on commercial projects. I live in Augsburg. I enjoy space, books, and dogs. I really want a border collie. My dream is to go to space and own a Porsche 992 GT3 RS, as well as have approximately 10 billion euros in my account. I'm a full-stack developer, primarily focusing on backend but also capable of working on frontend. My main programming languages are Python, Java, and JavaScript. I have also worked with Go and C++. I'm familiar with frameworks like Django, Flask, FastAPI, ReactJS, NodeJS, Gingonic, and Spring Boot, as well as tools like Netbox and Proxmox. I'm proficient with Linux distributions such as Ubuntu, Debian, CentOS, and Kali. I created this website to run my blog and showcase my projects. I love learning new things, especially blockchain and artificial intelligence. You can reach me at seinarukiro@gmail.com. I'm 20 years old. I'm fluent in English, Russian, and Ukrainian, and I speak German at a B2 level. Вопрос от пользователя - ";
     document.getElementById("user-input").value = "";
     displayMessage(userInput, true);
-    document.getElementById("typingIndicator").style.display = "block"; // Показываем сообщение "typing..."
+    document.getElementById("typingIndicator").style.display = "block";
 
-    var OPENAI_API_KEY = "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"; // Замените на ваш ключ API
+    var OPENAI_API_KEY = "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";
     var url = "https://api.openai.com/v1/chat/completions";
     var promptWithUser = userPrompt + userInput;
     var requestData = {
@@ -24,8 +24,8 @@ function sendMessage() {
     })
         .then(response => response.json())
         .then(data => {
-            displayMessage(data.choices[0].message.content); // Отображаем ответ от модели
-            document.getElementById("typingIndicator").style.display = "none"; // Убираем сообщение "typing..." после получения ответа
+            displayMessage(data.choices[0].message.content);
+            document.getElementById("typingIndicator").style.display = "none";
         })
         .catch(error => console.error('Error:', error));
 }
@@ -35,7 +35,6 @@ function displayMessage(message, isUser) {
     var chatMessage = document.createElement("div");
     chatMessage.className = "chat-message";
 
-    // Добавляем префикс к сообщению в зависимости от отправителя
     var prefix = isUser ? "You: " : "AI: ";
     chatMessage.textContent = prefix + message;
 
---
+++ frontend/themes/hugo-cat/README.md
@@ -0,0 +1,24 @@
+# hugo-cat
+
+Hugo v0.111.3
+
+
+# demo 
+
+[https://hugo-cat.pages.dev/](https://hugo-cat.pages.dev/)
+ 
+
+<img src="https://raw.githubusercontent.com/httpsecure/hugo-cat/main/images/tn.png" width=100% height=100%>
+
+
+This a hugo theme based on hugo red rose  and rewrite without bootstrap .
+
+Make it your own.
+
+If you like this themes dont forget ⭐ . 
+
+Also free to contribute making this theme complete, writing scheme json or dark mode etc.
+
+## Installation
+
+Install Hugo and create a new site & dir struct. See [the Hugo documentation](https://gohugo.io/getting-started/quick-start/) for details.
